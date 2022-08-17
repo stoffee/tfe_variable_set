a39dd4c390ec48979ebe0fd13bfe73e2df4b65fe
stoffee <chris@roadkill.org>
2022-08-17 14:02:01 -0700

    tfe_variable_set testing

---
+++ os_tf/chris.tf
@@ -0,0 +1,37 @@
+terraform {
+  cloud {
+    organization = "cdunlap"
+    workspaces {
+      name = "tfe_variable_set"
+    }
+  }
+}
+
+terraform {
+  required_providers {
+    tfe = {
+      source  = "hashicorp/tfe"
+      version = "0.36.0"
+    }
+  }
+}
+
+data "tfe_organization" "foo" {
+  name = "cdunlap"
+}
+
+output "org" {
+  description = "Org Name"
+  value       = data.tfe_organization.foo.name
+}
+
+output "email" {
+  description = "Org Email"
+  value       = data.tfe_organization.foo.email
+}
+
+resource "tfe_variable_set" "test" {
+  name         = "Test Varset"
+  description  = "Some description."
+  organization = data.tfe_organization.foo.name
+}
---
+++ remote_backend_test/docs.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_providers {
+    tfe = {
+      source  = "hashicorp/tfe"
+      version = "0.36.0"
+    }
+  }
+}
+
+resource "tfe_workspace" "test" {
+  name         = "my-workspace-name"
+  organization = "cdunlap"
+}
+
+resource "tfe_variable_set" "test" {
+  name          = "Test Varset"
+  description   = "Some description."
+  organization  = "cdunlap"
+}
+
+resource "tfe_workspace_variable_set" "test" {
+  workspace_id    = tfe_workspace.test.id
+  variable_set_id = tfe_variable_set.test.id
+}
+
+resource "tfe_variable" "test-a" {
+  key             = "seperate_variable"
+  value           = "my_value_name"
+  category        = "terraform"
+  description     = "a useful description"
+  variable_set_id = tfe_variable_set.test.id
+}
+
+resource "tfe_variable" "test-b" {
+  key             = "another_variable"
+  value           = "my_value_name"
+  category        = "env"
+  description     = "an environment variable"
+  variable_set_id = tfe_variable_set.test.id
+}
